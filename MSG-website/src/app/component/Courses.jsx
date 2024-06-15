056676bd2fbf4424857b6131890625545c3f0a12
Kulmeetcodez <singhkulmeet3@gmail.com>
2024-06-15 22:37:36 +0530

    Courses data updated

--- MSG-website/src/app/component/Courses.jsx
+++ MSG-website/src/app/component/Courses.jsx
@@ -1,17 +1,33 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
-function Card({ title, img }) {
+function Card({ title, img, content }) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="flex flex-col items-center relative cursor-pointer group">
+    <div
+      className="flex flex-col items-center relative cursor-pointer group"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="flex items-center justify-center bg-gradient-to-b from-[#2B02B8] to-[#D4C2FF] transform -rotate-45 absolute -top-12 rounded-lg ">
         <div className="flex items-center p-2 sm:p-4 transform rotate-45">
           <Image src={img} alt="drawing" width={30} height={30} />
         </div>
       </div>
-      <div className="flex items-center justify-center bg-primary-lightPurple rounded-ss-3xl rounded-b-none rounded-e-none transition-all duration-300 group-hover:rounded-br-3xl group-hover:pb-32 ">
+      <div className="flex flex-col items-center justify-center bg-primary-lightPurple rounded-ss-3xl rounded-b-none rounded-e-none transition-all duration-300 group-hover:rounded-br-3xl ">
         <p className="text-black text-base sm:text-xl p-2 mx-3 sm:mx-6 my-2 sm:my-4 font-mono">
           {title}
         </p>
+        <div
+          className={`w-48 flex items-center justify-center text-black transition-all duration-300 ease-in-out -mt-2 ${
+            isHovered ? "h-auto" : "h-0"
+          } overflow-hidden`}
+        >
+          <p className="text-xs sm:text-sm font-mono font-semibold py-4 px-2">
+            {content}
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -35,11 +51,31 @@ export default function Courses() {
       </h1>
 
       <div className="flex flex-row flex-wrap items-start justify-center gap-16 sm:gap-6 font-bold sm:h-40 ">
-        <Card title="Drawing Courses" img="/assets/drawing.svg" />
-        <Card title="Painting Courses" img="/assets/paint.svg" />
-        <Card title="Mentorship" img="/assets/mentor.svg" />
-        <Card title="Customized Arts" img="/assets/custom-arts.svg" />
-        <Card title="Art Materials" img="/assets/architecture.svg" />
+        <Card
+          title="Drawing Courses"
+          img="/assets/drawing.svg"
+          content="Enroll in our drawing courses to refine your skills and express your creativity."
+        />
+        <Card
+          title="Painting Courses"
+          img="/assets/paint.svg"
+          content="Join our painting courses to master techniques and unleash your artistic potential."
+        />
+        <Card
+          title="Mentorship"
+          img="/assets/mentor.svg"
+          content="Empowering aspiring artists through personalized mentorship, fostering creativity and skill development."
+        />
+        <Card
+          title="Customized Arts"
+          img="/assets/custom-arts.svg"
+          content="Commission personalized artworks that capture your unique vision and style."
+        />
+        <Card
+          title="Art Materials"
+          img="/assets/architecture.svg"
+          content="Explore a curated selection of premium art materials to fuel your creativity."
+        />
       </div>
     </div>
   );
